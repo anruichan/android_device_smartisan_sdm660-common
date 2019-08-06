a35e85178b8c05ac7fb9073b50bfe54117ffdc31
Bruno Martins <bgcngm@gmail.com>
2019-08-06 15:11:14 +0100

    sdm660-common: Delete class_main init shell script
    Apart from useless battery capacity faking, all this does is to
    start/stop data and radio services based upon the baseband type
    of several QCOM targets. Since all the supported targets in this
    common tree are of the same type, simply start all relevant services
    unconditionally and get rid of one more shell script.
    
    Change-Id: I1297b2537fcbea31d514bab5e248691cabdf523c

--- common.mk
+++ common.mk
@@ -249,7 +249,6 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.class_main.sh \
     init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
