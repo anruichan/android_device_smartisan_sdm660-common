7e9d2f4d700068ebcb253205b88b85a98d1d4c67
Bruno Martins <bgcngm@gmail.com>
2019-09-14 22:00:15 +0100

    sdm660-common: Remove obsolete RIL variant board flag
    There's no CAF variant anymore.
    
    Change-Id: Ief74f8b2aa861b0d16ffab9b52304006008d2444

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -209,7 +209,6 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_smartisan
 
 # RIL
-TARGET_RIL_VARIANT := caf
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_LD_SHIM_LIBS += /system/lib/lib-imsvideocodec.so|/system/lib/libshim_ims.so
