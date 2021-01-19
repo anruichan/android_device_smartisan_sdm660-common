5dc46d6b64b50d5cf4672d5dbc8003732c12327f
Bharat Pawar <bpawar@codeaurora.org>
2021-01-19 19:49:14 +0530

    sdm660-common: Enable LED based vibrator
    Enable LED based vibrator for sdm660 targets.
    
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- common.mk
+++ common.mk
@@ -417,7 +417,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    vendor.qti.hardware.vibrator.service
 
 # VNDK
 PRODUCT_COPY_FILES += \
