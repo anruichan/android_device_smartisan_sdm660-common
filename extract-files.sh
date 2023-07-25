6fb169e2ed38887c0bc497489d4f6eb6d2b78a48
anrui2032 <anrui2032@gmail.com>
2023-07-25 05:47:26 +0000

    sdm660-common: extract-files.sh: Replace $SRC_QC with $SRC
    Change-Id: I1005270360eb9c775909afa352d4b147b6d1cfeb

--- extract-files.sh
+++ extract-files.sh
@@ -34,7 +34,6 @@ fi
 CLEAN_VENDOR=true
 
 SRC=$1
-SRC_QC=$2
 
 if [ -z "$SRC" ]; then
     SRC=adb
@@ -44,8 +43,8 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC_QC" "$SECTION"
-extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC_QC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC" "$SECTION"
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
