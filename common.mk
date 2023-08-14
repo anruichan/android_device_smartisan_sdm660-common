6caf9c7ceb4eb8bfa7d6ff91a611db3c2ecea25c
anrui2032 <anrui2032@gmail.com>
2023-08-14 08:33:29 +0000

    Revert "sdm660: Shims lib-imsvideocodec.so"
    This reverts commit 2670b5d7511401152c185676f01d81c87ed69298.
    
    Change-Id: I9f378d7f3008dbef49985447726927b5e721620c

--- common.mk
+++ common.mk
@@ -345,7 +345,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    libshim_ims \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
