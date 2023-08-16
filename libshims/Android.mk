DD3Boh/android_device_xiaomi_sdm660-common

sdm660-common: Make shim for libcamera.sdm660.so
P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/vendor/lib/hw/camera.sdm660.so"

Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

libshims/Android.mk
@@ -1,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := camera_sdm660_shim.cpp
+LOCAL_SHARED_LIBRARIES := libui libutils
+LOCAL_MODULE := libcamera_sdm660_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)

anruichan/android_device_smartisan_sdm660-common

libshims/Android.mk
@@ -35,6 +35,14 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := camera_sdm660_shim.cpp
+LOCAL_SHARED_LIBRARIES := libui libutils
+LOCAL_MODULE := libshim_camera_sdm660
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := ims_shim.cpp
 