166fb93e4957dd01784bb391b4cb6162752b2779
anrui2032 <anrui2032@gmail.com>
2023-08-21 07:53:03 +0000

    sdm660-common: Remove android.hidl.manager@1.0-java
     * Built by default
    
    Change-Id: I49295158c36ac2ce58a80814fda2c8d81b27ebe0

--- common.mk
+++ common.mk
@@ -247,8 +247,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0
 
 PRODUCT_PACKAGES += \
     libhidltransport \
