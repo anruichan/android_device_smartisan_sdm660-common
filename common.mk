8be6689ca9b5f9f788f95e4b34caf16f23c2d2ee
anrui2032 <anrui2032@gmail.com>
2023-08-29 07:35:15 +0000

    sdm660-common: Build android.hardware.radio.deprecated@1.0 HAL
    Change-Id: Iddd6f5213b1fd2d1fe67fd544a3f8c2e34e4a5cc

--- common.mk
+++ common.mk
@@ -368,6 +368,7 @@ PRODUCT_PACKAGES += \
     libshim_ims \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
